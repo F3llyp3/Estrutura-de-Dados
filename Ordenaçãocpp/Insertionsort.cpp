56bb0dda65acc93452ccc1d1182bd5f1b13d7ef5
Fellype <fellypeh4@gmail.com>
2024-07-30 12:15:30 -0300

    enviando tudo

--- Ordenaçãocpp/Insertionsort.cpp
+++ Ordenaçãocpp/Insertionsort.cpp
@@ -1,17 +1,40 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
 
 using namespace std;
 int main(void){
-ifstream num("C:/Users/felly/Documents/Estrutura de dados/Ordenaçãocpp/entrada.txt");
-string linha;
-//num.open("entrada.txt");
-// comentário teste
+    ifstream num;
+    string linha;
+    vector <int> vetor;
+    
+    num.open("../entrada.txt");
+    
 
-if(num.is_open()){
-    while(getline(num, linha)){
-        cout << linha << endl;
+    if(num.is_open()){
+        while(getline(num, linha)){
+            int n = stoi(linha);
+            vetor.push_back(n);
+        }
     }
+    else{ cout << "Nao foi possivel abrir o arquivo" << endl;}
+
+    int aux =0;
+
+    for (int i = 1; i<vetor.size(); i++){ 
+        for(int j=i; j>0; j--){
+            if (vetor[j] < vetor[j-1]){
+                aux = vetor[j];
+                vetor[j]= vetor[j-1];
+                vetor[j-1] = aux;
+        }
+        }
+    }
+    
+    for(int i =0; i<vetor.size(); i++){
+        cout << vetor[i] << endl;
+    }
+    
+    return 0;
 }
-else{ cout << "Nao foi possivel abrir o arquivo" << endl;}
-}
+    
